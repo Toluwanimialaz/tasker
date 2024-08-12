78ca398544c316f61f339ab3aee2c49b9f9ec4c5
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-12 20:56:11 +0100

    edited vite config proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiURL=import.meta.env.VITE_API_URL
+
 // https://vitejs.dev/confin m,.;''g/
 export default defineConfig({
   server:{
     proxy:{
-      "/api":`${apiURL}`
+      "/api":'https://tasker-backend-mu.vercel.app/'
     }
   },
   plugins: [react()]
