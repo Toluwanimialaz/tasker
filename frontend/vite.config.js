c0c7b0e05e7808e6a9508cdc8d8ed8d31ae35fa0
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-12 20:36:20 +0100

    edited vite config proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const apiURL=import.meta.env.VITE_API_URL
-// https://vitejs.dev/config/
+// https://vitejs.dev/confin m,.;''g/
 export default defineConfig({
   server:{
     proxy:{
