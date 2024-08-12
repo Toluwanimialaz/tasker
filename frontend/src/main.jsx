01aa379763eeb1572e0ff39a3426da41eaa053a9
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-12 20:43:20 +0100

    edited vite config proxy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </BrowserRouter>
 )
 
-//remember to re-add " <React.StrictMode>" as a parent to "<BrowserRouter/>" because <React.StrictMode> causes a rerender(which u don't want for this particular task page) in testing but not in production
+//haha loolz remember to re-add " <React.StrictMode>" as a parent to "<BrowserRouter/>" because <React.StrictMode> causes a rerender(which u don't want for this particular task page) in testing but not in production
