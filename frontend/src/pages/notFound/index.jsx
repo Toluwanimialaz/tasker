78d2f46544c4eb149c7f90cbe88aed907528f276
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-13 16:44:36 +0100

    edited vite config proxy

--- frontend/src/pages/notFound/index.jsx
+++ frontend/src/pages/notFound/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function not(){
     return(
         <>
-         <h1>This page does not exist</h1>
+         <h1>This page does Not exist</h1>
         </>
     )
 }
