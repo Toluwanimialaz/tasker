007b7567e9ed4f8c3edb507524f5884fceefafd0
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-20 02:25:47 +0100

    frontend editing

--- backend/server.js
+++ backend/server.js
@@ -54,11 +54,11 @@ const corsOptions = {
   
 app.use(cors(corsOptions));
 
-app.options('*', (req, res) => {
+app.use( (req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://tasker-client-beige.vercel.app');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-HTTP-Method-Override, Accept');
     res.sendStatus(200);
 });
 
