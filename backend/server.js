1286f24dff283723301c3142a4553f01d06a302c
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-20 02:34:29 +0100

    frontend editing

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,6 @@ app.use( (req,res,next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-HTTP-Method-Override, Accept');
-    res.sendStatus(200);
     next()
 });
 
