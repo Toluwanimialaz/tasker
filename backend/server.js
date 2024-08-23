0fb547b16115886c4cb7668aec692b804c4f914b
Toluwanimialaz <toluwanimiala28@gmail.com>
2024-08-23 22:54:42 +0100

    frontend editing

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.get("/",(req,res)=>{
     res.status(200).json({"working":"successs"})
 })
 
-app.get("/api",auhenticated,cors(corsOptionss),(req,res)=>{
+app.get("/api",auhenticated,(req,res)=>{
     res.json({names:req.user.name})
 })
 
